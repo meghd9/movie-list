7718813f1d8a95e38dac6e7c00a1ec292b36ce25
Megh <meghd9@gmail.com>
2025-07-20 13:24:50 +0100

    Creating MovieCard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import "./App.css";
+import MovieCard from "./components/MovieCard";
 
 function App() {
-  return <div></div>;
+  return (
+    <>
+    <div>
+      <MovieCard movie={{ title: "Film", release_date: 2024 }} />
+      <MovieCard movie={{ title: "Another film", release_date: 2024 }} />
+    </div>
+    </>
+  );
 }
 
 export default App;
